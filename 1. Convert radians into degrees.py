b6119e29c04f3ed819ccc5260cfaef9f797b3113
Poorna Shri <poornashri2703@gmail.com>
2023-07-29 13:55:34 +0530

    Radian into Degrees

---
+++ 1. Convert radians into degrees.py
@@ -0,0 +1,15 @@
+'''
+Write a function in Python that accepts one numeric parameter. This parameter will be the measure of an angle in radians. The function should convert the radians into degrees and then return that value.					
+'''
+
+#Solution
+pi=3.141592653589793;
+
+def radians_to_degrees(radians):
+    degrees = radians * (180 / pi)
+    return degrees
+
+if __name__ == '__main__':
+    radians_input = float(input("Enter the angle in radians: "))
+    degrees_output = radians_to_degrees(radians_input)
+    print(f"{radians_input} radians is approximately {degrees_output} degrees.")
